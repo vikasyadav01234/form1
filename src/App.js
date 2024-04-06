2e799119cd661759e8a8b83d38a346749368ee1f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-06 10:35:19 +0530

    main

--- src/App.js
+++ src/App.js
@@ -115,6 +115,8 @@ function App() {
         onChange={changeHandler}
         className="outline"
       />
+      <br/>
+      <br/>
       <fieldset>
         <legend>By Email</legend>
         <div className="flex">
